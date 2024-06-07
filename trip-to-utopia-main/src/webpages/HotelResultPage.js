45d2e1ce42c4391ed6cf79544e06dfcfd5c35f9e
Aneesh.M <71017003+Aneesh-Malapaka@users.noreply.github.com>
2024-06-07 10:24:39 +0530

    Update HotelResultPage.js

--- trip-to-utopia-main/src/webpages/HotelResultPage.js
+++ trip-to-utopia-main/src/webpages/HotelResultPage.js
@@ -5,8 +5,6 @@ import { useLocation } from "react-router-dom";
 import React from "react";
 
 export default function HotelResultPage(props) {
-  var hotels = [];
-
   const [state, setState] = useState({
     area_name: "",
     date_in: "",
@@ -15,32 +13,33 @@ export default function HotelResultPage(props) {
     children: "",
     rooms: "",
   });
+
   const { area_name, date_in, date_out, adults, children, rooms } = state;
-
   const location = useLocation();
+  const [hotel, setHotel] = useState([]);
 
-  function get_hotels(dest_id) {
-    let destID = dest_id;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
-        "X-RapidAPI-Key": "9b03cfc029msh2ebb52c2f5c005cp1299f3jsn8fe51ec8b8de",
-      },
+  useEffect(() => {
+    const get_hotels = (dest_id) => {
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "9b03cfc029msh2ebb52c2f5c005cp1299f3jsn8fe51ec8b8de",
+        },
+      };
+
+      fetch(
+        `https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date=${state.date_out}&units=metric&dest_id=${dest_id}&dest_type=city&locale=en-us&adults_number=${state.adults}&order_by=popularity&filter_by_currency=INR&checkin_date=${state.date_in}&room_number=${state.rooms}&children_number=${state.children}&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&include_adjacency=true`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setHotel(response.result);
+        })
+        .catch((err) => console.error(err));
     };
 
-    fetch(
-      `https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date=${state.date_out}&units=metric&dest_id=${destID}&dest_type=city&locale=en-us&adults_number=${state.adults}&order_by=popularity&filter_by_currency=INR&checkin_date=${state.date_in}&room_number=${state.rooms}&children_number=${state.children}&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&include_adjacency=true`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        setHotel(response.result);
-      })
-      .catch((err) => console.error(err));
-  }
-  useEffect(() => {
-    // console.log(location.state);
     setState({ ...location.state, area_name: location.state.area_name });
 
     if (location.state.area_name.length > 0) {
@@ -61,26 +60,22 @@ export default function HotelResultPage(props) {
         .then((response) => {
           var destid = response[0].dest_id;
           get_hotels(destid);
-          fetch(
-            `https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date=${location.state.date_out}&units=metric&dest_id=${destid}&dest_type=city&locale=en-us&adults_number=${location.state.adults}&order_by=popularity&filter_by_currency=INR&checkin_date=${location.state.date_in}&room_number=${location.state.rooms}&children_number=${location.state.children}&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&include_adjacency=true`,
-            options
-          )
-            .then((response) => response.json())
-            .then((response) => {
-              console.log(response);
-              setHotel(response.result);
-            })
-            .catch((err) => console.error(err));
         })
         .catch((err) => console.error(err));
     } else {
       console.log("Wrong input");
     }
-  }, []);
+  }, [
+    location.state,
+    setState,
+    state.adults,
+    state.children,
+    state.date_in,
+    state.date_out,
+    state.rooms,
+  ]);
 
-  const [hotel, setHotel] = useState([]);
-
-  hotels = hotel.map((item) => {
+  const hotels = hotel.map((item) => {
     return <HotelCard key={item.hotel_id} item={item} />;
   });
 
